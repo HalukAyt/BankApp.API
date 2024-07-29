081cc46add0e5cc0c0d12ecd6f56a8fde629addd
Haluk Aytış <halukaytis@hotmail.com>
2024-07-29 15:30:21 +0300

    Cors Bağlantısı Eklendi

---
+++ BankApp.Persistence/Concrete(Service)/AccountService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.Persistence.Concrete_Service_
+{
+    public class AccountService
+    {
+    }
+}
